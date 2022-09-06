9e353e7fd24cbe9750c23422635536000eb8ae67
Cristian Villafañe <cristianv129@hotmail.com>
2022-09-05 22:33:44 -0300

    Agregado simple selector de cantidad a mostrar

--- app/src/main/java/com/tp/tpunla/activities/ActivityFilms.java
+++ app/src/main/java/com/tp/tpunla/activities/ActivityFilms.java
@@ -5,6 +5,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.tp.tpunla.R;
 import com.tp.tpunla.data.Data;
@@ -13,17 +17,36 @@ public class ActivityFilms extends AppCompatActivity {
 
     RecyclerView rvFilms;
     FilmAdapter filmAdapter;
+    Spinner dropdown;
+    String[] cantidades = new String[]{"10", "8", "6", "4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_films);
-        setupAdapter();
-    }
-
-    private void setupAdapter() {
+        dropdown = findViewById(R.id.filmsSpinner);
         rvFilms = findViewById(R.id.rv_films);
-        filmAdapter = new FilmAdapter(Data.getFilms(10), film -> {
+
+        setupAdapter(10);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, cantidades);
+        dropdown.setAdapter(adapter);
+
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                setupAdapter(Integer.parseInt(cantidades[position]));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    };
+
+    private void setupAdapter(int cantidad) {
+        rvFilms = findViewById(R.id.rv_films);
+        filmAdapter = new FilmAdapter(Data.getFilms(cantidad), film -> {
             Intent detailActivity = new Intent(ActivityFilms.this, ActivityFilmDetail.class);
             detailActivity.putExtra("id", film.getId());
             startActivity(detailActivity);
